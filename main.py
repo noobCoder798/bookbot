2bd8472bec772bfcd6f9d79209707169d58706a7
noobCoder798 <akral@trinityisd.net>
2025-03-15 10:20:09 -0500

    changed main coding

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+# Fetches content to read and stores it into container
+with open('/home/anthony-kral/Documents/GitHub/bookbot/books/frankenstein.txt', encoding="utf-8") as f:
+    read_data = f.read()
+    
+# checks if the file has closed correctly
+f.closed
+def main():
+    print(read_data)
+
+
+main()
